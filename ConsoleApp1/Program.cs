8df83046ba43c40e63100733708c093d6cd2c017
Gul Zaib <gul.zaib@lmkr.com>
2018-02-10 09:52:18 +0500

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Process[] processlist = Process.GetProcesses();
+                foreach (Process theprocess in processlist)
+                {
+                    Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
+                }
+                using (var client = new WebClient())
+                {
+                    string result = client.DownloadString("http://google.com");
+                    // TODO: do something with the returned content from the servlet
+                }
+            }
+        }
+    }
+}
